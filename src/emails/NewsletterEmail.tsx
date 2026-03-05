676a1080fe67be3a300c8493a556595c2a5df28e
Pierre Treton <pierre.treton3@gmail.com>
2026-03-05 12:49:08 +0100

    feat: add white logo

--- src/emails/NewsletterEmail.tsx
+++ src/emails/NewsletterEmail.tsx
@@ -75,8 +75,8 @@ export default function NewsletterEmail({ title, content }: Props) {
         {/* Header */}
         <div className="header-footer">
           <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", marginBottom: "24px", flexWrap: "wrap", gap: "16px" }}>
-            <img src="https://edtlab.fr/Logo_EDT_CBLOT.png" alt="Engineering Digital Twin" style={{ maxWidth: "200px", margin: 0 }} />
-            <img src="https://edtlab.fr/logo-fr-white.png" alt="Engineering Digital Twin" style={{ maxWidth: "200px", margin: 0 }} />
+            <img src="https://edtlab.fr/Logo_EDT_CBLOT-white.png" alt="Engineering Digital Twin" style={{ maxWidth: "300px", margin: 0 }} />
+            <img src="https://edtlab.fr/logo-fr-white.png" alt="Engineering Digital Twin" style={{ maxWidth: "300px", margin: 0 }} />
           </div>
           <h1 style={{ color: "white" }}>{title}</h1>
         </div>
